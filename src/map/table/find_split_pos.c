03fbae4a26290978fbf9eae13810970ef01b9cb0
mbores06 <207013292+mbores06@users.noreply.github.com>
2026-01-26 13:48:51 +0100

    norminette for find_split_pos

--- src/map/table/find_split_pos.c
+++ src/map/table/find_split_pos.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   find_split_pos.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lfiorell <lfiorell@student.42nice.fr>      +#+  +:+       +#+        */
+/*   By: mbores <mbores@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 13:22:50 by lfiorell          #+#    #+#             */
-/*   Updated: 2026/01/26 13:36:14 by lfiorell         ###   ########.fr       */
+/*   Updated: 2026/01/26 13:46:17 by mbores           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,46 +54,54 @@ static bool	line_is_option(const char *s, size_t len, t_opt_ctx *ctx)
 	return (false);
 }
 
+static bool	skip_empty_lines(const char *c, size_t *i)
+{
+	size_t	len;
+
+	len = line_len(c + *i);
+	if (!line_is_empty(c + *i, len))
+		return (false);
+	*i += len;
+	if (c[*i] == '\n')
+		(*i)++;
+	return (true);
+}
+
+static bool	handle_option_lines(const char *c,
+				size_t *i,
+				t_opt_ctx *ctx)
+{
+	size_t	len;
+
+	len = line_len(c + *i);
+	if (!line_is_option(c + *i, len, ctx))
+		return (false);
+	*i += len;
+	if (c[*i] == '\n')
+		(*i)++;
+	return (true);
+}
+
 bool	find_split_pos(const char *content, size_t *opt_end)
 {
 	t_opt_ctx	ctx;
 	size_t		i;
-	bool		options_complete;
-	bool		map_found;
-	size_t		len;
 
-	if (!content)
+	if (!content || !opt_end)
 		return (false);
 	ft_bzero(&ctx, sizeof(t_opt_ctx));
-	options_complete = false;
-	map_found = false;
 	i = 0;
 	while (content[i])
 	{
-		len = line_len(content + i);
-		if (line_is_empty(content + i, len))
-		{
-			i += len;
-			if (content[i] == '\n')
-				i++;
+		if (skip_empty_lines(content, &i))
 			continue ;
-		}
-		if (!options_complete)
-		{
-			if (!line_is_option(content + i, len, &ctx))
-				return (false);
-			options_complete = opt_ctx_complete(&ctx);
-			i += len;
-			if (content[i] == '\n')
-				i++;
-			continue ;
-		}
-		if (!map_found)
+		if (!handle_option_lines(content, &i, &ctx))
+			return (false);
+		if (opt_ctx_complete(&ctx))
 		{
 			*opt_end = i;
-			map_found = true;
+			return (true);
 		}
-		break ;
 	}
-	return (map_found);
+	return (false);
 }
